dda35a399228a7b616b59eb2d27257a26042938a
Solar_Tiger <davidjsingleton30@yahoo.com>
2024-12-17 15:28:15 -0800

    Add function that fetches country list and populates select element

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import './style.css';
 import './reset.css';
+
+const countrySelect = document.querySelector('#country');
+
+async function getCountries() {
+    fetch('https://countriesnow.space/api/v0.1/countries/positions')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data.data);
+
+            for (let i = 0; i < data.data.length; i++) {
+                const countryOption = document.createElement('option');
+
+                countryOption.value = data.data[i].name.toLowerCase();
+                countryOption.textContent = data.data[i].name;
+
+                countrySelect.appendChild(countryOption);
+            }
+        });
+}
+
+getCountries();
